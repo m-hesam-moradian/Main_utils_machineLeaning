83068766819410635761ff1e734b85202bc88e11
m.hesam moradian <m.hesam.moradian@gmail.com>
2025-11-06 09:59:18 +0330

    data randomised

--- data_manage/preprocessing/Data_randomization.py
+++ data_manage/preprocessing/Data_randomization.py
@@ -2,16 +2,16 @@ import pandas as pd
 
 # --- Load original dataset ---
 excel_path = r"C:\Users\Sam\Desktop\ML\task\BMM-EI. No.24-.xlsx"
-df = pd.read_excel(excel_path, sheet_name="ENN")
+df = pd.read_excel(excel_path, sheet_name="RUS")
 
 # --- Shuffle the dataset ---
 df_shuffled = df.sample(frac=1, random_state=42).reset_index(drop=True)
 df_shuffled.to_clipboard(index=False)
 # --- Save to same Excel file under new sheet ---
-# with pd.ExcelWriter(
-#     excel_path, engine="openpyxl", mode="a", if_sheet_exists="replace"
-# ) as writer:
-#     df_shuffled.to_excel(writer, sheet_name="DATA_Shuffled", index=False)
+with pd.ExcelWriter(
+    excel_path, engine="openpyxl", mode="a", if_sheet_exists="replace"
+) as writer:
+    df_shuffled.to_excel(writer, sheet_name="DATA_Shuffled", index=False)
 
 print("✅ Dataset randomized successfully.")
 print(f"📁 Shuffled data saved to sheet 'DATA_Shuffled' in '{excel_path}'.")
