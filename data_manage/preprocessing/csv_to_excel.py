1d66dafed59cc6595b72ca5926608220c3efe645
m.hesam moradian <m.hesam.moradian@gmail.com>
2025-11-18 11:09:54 +0330

    ofi created

--- data_manage/preprocessing/csv_to_excel.py
+++ data_manage/preprocessing/csv_to_excel.py
@@ -1,15 +1,16 @@
 import pandas as pd
 
-# خواندن فایل CSV
+# خواندن فایل CSV با جداکننده ;
 df = pd.read_csv(
-    r"D:\ML\Main_utils\task\EL. No 6. Allocated bandwidth- SVR-ENR-SCO-POA-GGO-DATA.csv"
+    r"C:\Users\Sam\Downloads\scarcity.csv",
+    sep=";"
 )
 
 # ذخیره به صورت فایل اکسل
 df.to_excel(
-    r"D:\ML\Main_utils\task\EL. No 6. Allocated bandwidth- SVR-ENR-SCO-POA-GGO-DATA.xlsx",
-    sheet_name="DATA"
+    r"C:\Users\Sam\Downloads\scarcity.xlsx",
+    sheet_name="DATA",
     index=False,
 )
 
-print("✅ فایل dataset_invade.xlsx با موفقیت ساخته شد.")
+print("✅ فایل Excel با موفقیت ساخته شد.")
